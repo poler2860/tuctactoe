fe9d34d325bfd497ff458981790c0fd2637bcf79
polychronis <poler2860@gmail.com>
2022-05-17 14:41:29 +0300

    GUI Addition

---
+++ src/view/HallOfFame.java
@@ -0,0 +1,23 @@
+package view;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+import control.GameController;
+
+public class HallOfFame extends GamePanel{
+	
+	
+	public HallOfFame(GameController gc) {
+		super(gc);		
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		int x = this.getWidth()/2 - 50;
+		int y = this.getHeight()/2;		
+		g.drawString("Hall Of Fame", x, y);
+	}
+
+}
