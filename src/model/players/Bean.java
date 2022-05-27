0c54e429e538089bc0d7ac4183768fb58c975146
left <kalamarasleft@gmail.com>
2022-05-27 14:40:56 +0300

    bean

--- src/model/players/Bean.java
+++ src/model/players/Bean.java
@@ -1,4 +1,15 @@
 package model.players;
 
+import model.GameModel;
+
 public class Bean {
+GameModel Bm;
+    void Beanplacemt() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                Bm.makeMove(i, j);
+
+            }
+        }
+    }
 }
