d0a3281fe0af0c302ae96f04c19a898a948b03f3
polychronis <poler2860@gmail.com>
2022-05-29 16:26:22 +0300

    Validity Checks fixes

--- src/model/players/Bean.java
+++ src/model/players/Bean.java
@@ -8,7 +8,18 @@ public class Bean extends player {
 GameModel gm;
 
 Random rand = new Random();
-    public void Beanplacement() {
-       while(gm.inPlay() && )
+    public void BeanPlacement() {
+        Boolean placedMark = true;
+        int row = 0;
+        int col = 0;
+        while(gm.inPlay() && !placedMark) {
+
+            row = getRandomNumber(0,2);
+            col = getRandomNumber(0,2);
+        }
+    }
+
+    public int getRandomNumber(int min, int max) {
+        return (int) (Math.random() * (max-min) + min);
     }
 }
